e8b2a5988b22497e7fc64deb9a57c83a33af4298
Ehsan <ehcan@icloud.com>
2020-08-20 16:25:53 +0430

    add body-parser

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const axios = require('axios');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.all('*', async function (req, res) {
 	try {
