760409fe12dfb90c8612781ea307fd1a5d8cdd2c
Ehsan <ehcan@icloud.com>
2020-08-20 16:01:20 +0430

    Telegram Bridge v1

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
+
+app.all('*', async function (req, res) {
+	try {
+		const result = await axios({
+			timeout: 5000,
+			url: 'https://api.telegram.org' + req.url,
+			method: req.method,
+			params: req.params,
+			data: req.body,
+		});
+
+		res.send(result.data);
+	} catch (err) {
+		res.send(err.response.data);
+	}
+});
+
+app.listen(port, () => {
+	console.log(`Telegram Bridge listening at http://127.0.0.1:${port}`);
+});
